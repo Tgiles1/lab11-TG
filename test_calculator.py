f54f094c3df5c2968774efcc2fa2d0c9fa3f4994
Tim Giles <inkyfrizzle29@gmail.com>
2025-11-18 17:25:49 -0500

    moved

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,10 @@
+#https://github.com/Tgiles1/lab11-TG.git
+#Partner 1: Tim Giles
+
 import unittest
 from calculator import *
 
-#https://github.com/Tgiles1/lab11-TG.git
-#Partner 1: Tim Giles
+
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
@@ -15,15 +17,15 @@ class TestCalculator(unittest.TestCase):
 
     ######## Partner 1
     def test_multiply(self):  
-        self.assertEqual(multiply(2, 3), 6)  
-        self.assertEqual(multiply(-4, 5), -20) 
-        self.assertEqual(multiply(0, 100), 0)      
+        self.assertEqual(mul(2, 3), 6)  
+        self.assertEqual(mul(-4, 5), -20) 
+        self.assertEqual(mul(0, 100), 0)      
 
     def test_divide(self): 
-        self.assertEqual(divide(2, 10), 5)        
-        self.assertEqual(divide(4, 1), 0.25) 
+        self.assertEqual(div(2, 10), 5)        
+        self.assertEqual(div(4, 1), 0.25) 
         with self.assertRaises(ZeroDivisionError): 
-            divide(0, 5)
+            div(0, 5)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion

2de615a08bbca93920882cd52d79465b3bc58fe1
Tim Giles <inkyfrizzle29@gmail.com>
2025-11-18 17:20:10 -0500

    calculator tested

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,9 @@
 import unittest
 from calculator import *
 
+#https://github.com/Tgiles1/lab11-TG.git
+#Partner 1: Tim Giles
+
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
@@ -11,12 +14,16 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):  
+        self.assertEqual(multiply(2, 3), 6)  
+        self.assertEqual(multiply(-4, 5), -20) 
+        self.assertEqual(multiply(0, 100), 0)      
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): 
+        self.assertEqual(divide(2, 10), 5)        
+        self.assertEqual(divide(4, 1), 0.25) 
+        with self.assertRaises(ZeroDivisionError): 
+            divide(0, 5)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -33,23 +40,20 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
     
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(0, 0), 0)
+        self.assertEqual(hypotenuse(-3, 4), 5)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            square_root(-1)
+        self.assertEqual(square_root(0), 0)
+        self.assertEqual(square_root(9), 3)
 
 # Do not touch this
 if __name__ == "__main__":
